fb1d915e3d4d9b532fdd1fc4566e43ffc40905ab
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-12-20 01:16:28 +0900

    プロジェクト作成

---
+++ src/main/java/plugin/minecraftenemydown/app/MinecraftEnemyDownAppApplication.java
@@ -0,0 +1,13 @@
+package plugin.minecraftenemydown.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftEnemyDownAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftEnemyDownAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plugin/minecraftenemydown/app/MinecraftEnemyDownAppApplicationTests.java
@@ -0,0 +1,13 @@
+package plugin.minecraftenemydown.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftEnemyDownAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
